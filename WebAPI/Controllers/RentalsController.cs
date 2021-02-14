3b8a50a9e1eb2cb857ae0b98c378e2c97262b255
ugurkurekci <ugurkurekci98@gmail.com>
2021-02-15 00:19:56 +0300

    ControllerAdded

---
+++ WebAPI/Controllers/RentalsController.cs
@@ -0,0 +1,73 @@
+﻿using Business.Abstract;
+using Entities.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RentalsController : ControllerBase
+    {
+        IRentalsServices _rentalsServices;
+
+        public RentalsController(IRentalsServices rentalsServices)
+        {
+            _rentalsServices = rentalsServices;
+        }
+        [HttpGet("getall")]
+        public IActionResult GetAll()
+        {
+            var result = _rentalsServices.GetAll();
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+        [HttpGet("getbyid")]
+        public IActionResult GetById(int id)
+        {
+            var result = _rentalsServices.GetById(id);
+            if (result.Success)
+            {
+                return Ok(result.Data);
+            }
+            return BadRequest(result);
+        }
+        [HttpPost("add")]
+        public IActionResult Add(Rentals rentals)
+        {
+            var result = _rentalsServices.Add(rentals);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+        [HttpPost("update")]
+        public IActionResult Update(Rentals rentals)
+        {
+            var result = _rentalsServices.Update(rentals);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+        [HttpPost("delete")]
+        public IActionResult Delete(Rentals rentals)
+        {
+            var result = _rentalsServices.Delete(rentals);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+    }
+}
